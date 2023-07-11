7f6c56c949018f7746f63fb084649b2e0752ccb8
Jack Wherry <jack@snazz.xyz>
2023-07-11 09:38:11 -0500

    Initial Commit

---
+++ FuzzyWeather/FuzzyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuzzyWeatherApp.swift
+//  FuzzyWeather
+//
+//  Created by Jack Wherry on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FuzzyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FuzzyWeatherUITests/FuzzyWeatherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FuzzyWeatherUITestsLaunchTests.swift
+//  FuzzyWeatherUITests
+//
+//  Created by Jack Wherry on 7/11/23.
+//
+
+import XCTest
+
+final class FuzzyWeatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
